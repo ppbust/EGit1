76832e7d5d004f4ae2760dd598c09e0532f655a6
ppbust <jl.bustamante@alumnos.upm.es>
2013-10-19 18:15:12 +0200

    Se ha implementado el método getSuma en Suma.java

--- src/spai/egit1/Suma.java
+++ src/spai/egit1/Suma.java
@@ -1,13 +1,11 @@
 package spai.egit1;
 
 public class Suma {
-
-    public double incrementa(double d) {
-        // TODO Auto-generated method stub
-        return 0;
+    public double getSuma(double a, double b) {
+        return a + b;
     }
 
-    public double getSuma(double d, double e) {
+    public double incrementa(double d) {
         // TODO Auto-generated method stub
         return 0;
     }
