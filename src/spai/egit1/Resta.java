2d718c11c966abbd65ed0ff5400a35aa91c0e829
ppbust <jl.bustamante@alumnos.upm.es>
2013-10-19 19:12:02 +0200

    Se han desarrollado los métodos en Resta.java

--- src/spai/egit1/Resta.java
+++ src/spai/egit1/Resta.java
@@ -1,15 +1,10 @@
 package spai.egit1;
 
 public class Resta {
-
-    public double decrementa(double d) {
-        // TODO Auto-generated method stub
-        return 0;
+    public double decrementa(double a) {
+        return a - 1;
     }
-
-    public double getDiferencia(double d, double e) {
-        // TODO Auto-generated method stub
-        return 0;
+    public double getDiferencia(double a, double b) {
+        return a - b;
     }
-
 }
