2289b54107b33c463312823009a477d1e9ba7a94
ppbust <jl.bustamante@alumnos.upm.es>
2013-10-19 19:10:36 +0200

    Se ha desarrollado el TestResta.java

--- src/spai/egit1/Resta.java
+++ src/spai/egit1/Resta.java
@@ -2,4 +2,14 @@ package spai.egit1;
 
 public class Resta {
 
+    public double decrementa(double d) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public double getDiferencia(double d, double e) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
 }
--- test/spai/egit1/TestResta.java
+++ test/spai/egit1/TestResta.java
@@ -1,5 +1,25 @@
 package spai.egit1;
 
-public class TestResta {
+import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+public class TestResta {
+    private Resta resta;
+    @Before
+    public void paraEjecutarAntes() throws Exception {
+        resta = new Resta();
+    }
+    @Test
+    public void aVerSiRestaBien() {
+        assertEquals("Test decrementa", 5.0, resta.decrementa(6.0), 1e-6);
+    }
+    @Test
+    public void aVerSiSacaLaDiferencia() {
+        assertEquals("Test resta", 5.0, resta.getDiferencia(8.0, 3.0), 1e-6);
+    }
+    @After
+    public void paraEjecutarDespues() throws Exception {
+    }
 }
