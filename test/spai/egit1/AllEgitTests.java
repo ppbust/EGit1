8dbd65af865217190f865994c1be8290970730d8
ppbust <jl.bustamante@alumnos.upm.es>
2013-10-19 19:22:49 +0200

    Se ha creado la suite de tests

---
+++ test/spai/egit1/AllEgitTests.java
@@ -0,0 +1,11 @@
+package spai.egit1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestSuma.class, TestResta.class})
+public class AllEgitTests {
+
+}
